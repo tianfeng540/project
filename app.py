ea509b0a7cc3c877f774d2bdfc5ee9d2b34b0e0f
Brucer <3250397353@qq.com>
2020-02-25 16:04:13 +0800

    添加首页模板

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "<h1>这是我的项目首页<h1>"
+    name = 'Brucer'
+    movies =[
+        {'title':'杀破狼','year':'2003'},
+        {'title':'扫毒','year':'2018'},
+        {'title':'捉妖记','year':'2016'},
+        {'title':'囧妈','year':'2020'},
+        {'title':'葫芦娃','year':'1989'},
+        {'title':'玻璃盒子','year':'2020'},
+        {'title':'调酒师','year':'2020'},
+        {'title':'釜山行','year':'2017'},
+        {'title':'导火索','year':'2005'},
+        {'title':'叶问','year':'2015'}
+    ]
+    return render_template('index.html',name=name,movies=movies)
