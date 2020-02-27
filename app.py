093d4925ec2c6662d28f309c9bfdfff2b4328636
Brucer <3250397353@qq.com>
2020-02-27 13:53:08 +0800

    '登陆'

--- app.py
+++ app.py
@@ -2,8 +2,11 @@ import os
 import sys
 import click
 
+
 from flask import Flask, render_template, request, url_for, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import LoginManager,UserMixin,login_user,logout_user,login_required,current_user,login_manager
 
 app = Flask(__name__)
 
@@ -68,6 +71,37 @@ def forge():
     db.session.commit()
     click.echo('数据导入完成')
 
+# 生成admin账号的函数
+@app.cli.command()
+@click.option('--username',prompt=True,help="用来登录的用户名")
+@click.option('--password',prompt=True,hide_input=True,confirmation_prompt=True,help="用来登录的密码")
+def admin(username,password):
+    db.create_all()
+    user = User.query.first()
+    if user is not None:
+        click.echo('更新用户')
+        user.username = username
+        user.set_password(password)
+    else:
+        click.echo('创建用户')
+        user = User(username=username,name="雷洛")
+        user.set_password(password)
+        db.session.add(user)
+    
+    db.session.commit()
+    click.echo('创建管理员账号完成')
+
+
+# Flask-login 初始化操作
+login_manager = LoginManager(app)   # 实例化扩展类
+@login_manager.user_loader
+def load_user(user_id):   # 创建用户加载回调函数，接受用户ID作为参数
+    user = User.query.get(int(user_id))
+    return user
+
+#
+login_manager.login_view = 'login'
+login_manager.login_message = "没有登录"
 
 # 首页
 @app.route('/', methods=['GET', 'POST'])
@@ -113,6 +147,23 @@ def edit(movie_id):
         return redirect(url_for('index'))
     return render_template('edit.html', movie=movie)
 
+@app.route('/settings',methods=['GET','POST'])
+@login_required
+def settings():
+    if request.method == 'POST':
+        name = request.form['name']
+
+        if not name or len(name)>20:
+            flash('输入错误')
+            return redirect(url_for('settings'))
+        
+        current_user.name = name
+        db.session.commit()
+        flash('设置name成功')
+        return redirect(url_for('index'))
+
+    return render_template('settings.html')
+
 
 # 删除信息
 @app.route('/movie/delete/<int:movie_id>', methods=['POST'])
@@ -129,6 +180,33 @@ def delete(movie_id):
 def page_not_found(e):
     # user = User.query.first()
     return render_template('404.html'), 404
+
+# 用户登录 flask提供的login_user()函数
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if not username or not password:
+            flash('输入错误')
+            return redirect(url_for('login'))
+        user = User.query.first()
+        if username == user.username and user.validate_password(password):
+            login_user(user)  # 登录用户
+            flash('登录成功')
+            return redirect(url_for('index'))  # 登录成功返回首页
+        flash('用户名或密码输入错误')
+        return redirect(url_for('login'))
+    return render_template('login.html')
+
+# 用户登出
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('退出登录')
+    return redirect(url_for('index'))
 
 
 # 模板上下文处理函数，在多个模板内都需要使用的变量
