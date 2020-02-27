5fba641fe4995d228689c6e4c00f637077e7f20e
Brucer <3250397353@qq.com>
2020-02-27 20:22:51 +0800

    'Done'

--- blog/__init__.py
+++ blog/__init__.py
@@ -14,13 +14,11 @@ else:
     prefix = 'sqlite:////'  # Mac，Linux，四个斜杠
 
 # 配置
-app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path),os.getenv('DATABASE_FILE','data.db'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
-app.config['SECRET_KEY'] = 'dev'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY','dev')
 
 db = SQLAlchemy(app)
-
-
 
 
 # Flask-login 初始化操作
